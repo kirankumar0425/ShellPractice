5ebac50b2b8e114954f068fbb7a4fd3a48598de9
kirankumar <kirannamshamgari2020@gmail.com>
2025-05-18 21:47:02 +0530

    hello world created

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+PERSON1=$1 #there should be no space before and after =
+PERSON2=$2
+
+
+echo "$PERSON1:: Hey musk, How are you?"
+echo "$PERSON2:: Hello Trump, I am fine. How are you doing"
+echo "$PERSON1: I am fine too. What's up?"
+echo "$PERSON2:: Nothing, just going to Mars now, will you come?"
+echo "$PERSON1:: Sorry, you carry on! I will come once you come back"
