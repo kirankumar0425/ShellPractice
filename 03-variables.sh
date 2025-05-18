c793059036342096b945314553236896ca85f323
kirankumar <kirannamshamgari2020@gmail.com>
2025-05-18 18:13:49 +0530

    hello world created

---
+++ 03-variables.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+
+echo "$PERSON1:: Hey musk, How are you?"
+echo "$PERSON2:: Hello Trump, I am fine. How are you doing"
+echo "$PERSON1: I am fine too. What's up?"
+echo "$PERSON2:: Nothing, just going to Mars now, will you come?"
+echo "$PERSON1:: Sorry, you carry on! I will come once you come back"
+
+
