6318e3b05594ffd24165834832f2444deff40fd7
kirankumar <kirannamshamgari2020@gmail.com>
2025-05-18 17:54:27 +0530

    hello world created

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+! /bin/bash
+
+echo "Trump:: Hey musk, How are you?"
+echo "Mush:: Hello Trump, I am fine. How are you doing"
+echo "Trump: I am fine too. What's up?"
+echo "Musk:: Nothing, just going to Mars now, will you come?"
+echo "Trump:: Sirry, you carry on! I will come once you come back"
